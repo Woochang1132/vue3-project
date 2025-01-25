11fe4f252552ca4fe8082324973aab98c9d2c503
Woochang <show4031132@gmail.com>
2025-01-25 17:49:12 +0900

    [개발] 상세 페이지 > 목록, 수정, 삭제 페이지로 갈 수 있도록 구성 진행중

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    <h2>게시글 상세</h2>
+    <h2>제목</h2>
+    <p>내용</p>
+    <p class="text-muted">2020-01-01</p>
     <hr class="my-4" />
-    <p>params : {{ $route.params }}</p>
-    <p>query : {{ $route.query }}</p>
-    <p>hash : {{ $route.hash }}</p>
+    <div class="row g-2">
+      <div class="col-auto">
+        <button class="btn btn-outline-dark">이전글</button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-outline-dark">다음글</button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-outline-dark">목록</button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-outline-dark">수정</button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-outline-dark">삭제</button>
+      </div>
+    </div>
   </div>
 </template>
 
