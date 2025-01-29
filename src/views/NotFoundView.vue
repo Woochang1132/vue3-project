f3ffb804dcc0e3aeb70b5ef35e3aab04799e396d
Woochang <show4031132@gmail.com>
2025-01-29 22:34:33 +0900

    [개발] 404NotFound 페이지 구성 완료

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,18 @@
 <template>
-  <div></div>
+  <div class="text-center py-5">
+    <h1>Oops!</h1>
+    <h2>404 Not Found</h2>
+    <div class="text-muted">요청한 페이지를 찾을 수 없습니다!</div>
+    <div class="mt-4">
+      <RouterLink to="/">
+        <button class="btn btn-primary">Home</button>
+      </RouterLink>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
 
 <style lang="scss" scoped></style>
