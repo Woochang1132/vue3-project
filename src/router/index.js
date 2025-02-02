9a42ba3baf46e31121c1c19d601e3f4b55780c0b
Woochang <show4031132@gmail.com>
2025-02-02 23:07:36 +0900

    [개발] 서버에 반영 시에는 history모드를 주로 사용하여, 원복

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import PostCreateView from '@/views/posts/PostCreateView.vue';
@@ -74,8 +74,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  // history: createWebHistory('/'),
-  history: createWebHashHistory(),
+  history: createWebHistory('/'),
+  // history: createWebHashHistory(),
   routes,
 });
 
