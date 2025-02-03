15509d5acc15d60162e163a5654e2b89d0fc374d
Choi Woochang <show4031132@gmail.com>
2025-02-03 18:20:15 +0900

    [개발] 게시글 등록 후, db에 해당 데이터 추가된 것을 확인할 수 있음

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -30,8 +30,12 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 const posts = ref([]);
 const fetchPosts = async () => {
-  const { data } = await getPosts();
-  posts.value = data;
+  try {
+    const { data } = await getPosts();
+    posts.value = data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 fetchPosts();
