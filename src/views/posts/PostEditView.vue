b81a1d2358a357b9b819afd062b86a9fbce2def2
Woochang1132 <show4031132@gmail.com>
2025-02-13 11:43:36 +0900

    [개발] vue, 내장 컴포넌트, TransitionGroup 활용해보기

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -18,7 +18,8 @@
         <button class="btn btn-primary">수정</button>
       </template>
     </PostForm>
-    <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
+    <!-- <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />-->
+    <AppAlert :items="alerts" />
   </div>
 </template>
 
@@ -57,24 +58,19 @@ fetchPost();
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    // router.push({ name: 'PostDetail', params: { id } });
     vAlert('수정이 완료되었습니다!!', 'success');
   } catch (error) {
     console.error(error);
+    vAlert(error.message);
   }
 };
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
 
-//alert
-const showAlert = ref(false);
-const alertMessage = ref('');
-const alertType = ref('');
+const alerts = ref([]);
 const vAlert = (message, type = 'error') => {
-  showAlert.value = true;
-  alertMessage.value = message;
-  alertType.value = type;
+  alerts.value.push({ message, type });
   setTimeout(() => {
-    showAlert.value = false;
+    alerts.value.shift();
   }, 2000);
 };
 </script>
