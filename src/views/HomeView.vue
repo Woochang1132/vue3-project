ddd6db2c70141fb4df319d70ed7c9ad3b513445c
Woochang1132 <show4031132@gmail.com>
2025-02-18 21:55:02 +0900

    [개발] toRef, toRefs 를 활용하여 반응형 유지하기

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,9 @@
     <AppGrid :items="items" v-slot="{ item }" col-class="col-6">
       <AppCard>{{ item }}</AppCard>
     </AppGrid>
+    <hr class="my-4" />
+    <h2>{{ position }}</h2>
+    <h2>x: {{ x }}, y: {{ y }}</h2>
   </div>
 </template>
 
@@ -24,7 +27,7 @@ export default {
 import { useRouter } from 'vue-router';
 // import AppGrid from '@/components/app/AppGrid.vue';
 // import AppCard from '@/components/app/AppCard.vue';
-import { ref } from 'vue';
+import { reactive, ref, toRef, toRefs } from 'vue';
 
 const router = useRouter();
 const goAboutPage = () => {
@@ -32,6 +35,23 @@ const goAboutPage = () => {
 };
 
 const items = ref(['사과', '딸기', '포도', '바나나']);
+
+const position = reactive({
+  x: 100,
+  y: 1000,
+});
+
+//const x = position.x; 이렇게 할당하면 x에 담긴 건 단순히 Number이고 반응형을 잃는다.
+//const x = ref(position.x) // 초기 값 100을 ref 함수에 할당한 것이지 reactive의 x와 동기화 된 것을 의미하지 않는다.
+/* 
+const { x, y } = position;
+console.log('x : ', typeof x);
+console.log('y : ', typeof y); 
+*/
+// const x = toRef(position, 'x');
+// const y = toRef(position, 'y');
+
+const { x, y } = toRefs(position);
 </script>
 
 <style lang="scss" scoped></style>
